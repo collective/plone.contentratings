277862dd85292ac6c722ae0409961b0cabea4e66
alecm <alecm@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-06-06 05:15:10 +0000

    Prepare plone.contentratings 1.0.1-1.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ description = read('README.txt') + '\n\n' + \
               read('docs', 'HISTORY.txt')
 
 setup(name='plone.contentratings',
-      version="1.0.1",
+      version="1.0.1-1",
       description="Plone support for the contentratings package",
       long_description=description,
       classifiers=[
