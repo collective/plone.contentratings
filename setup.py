3bdace5a30314f9e4c06f552a3f44121284ab818
Maurits van Rees <maurits@vanrees.org>
2013-09-20 23:55:23 +0200

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ description = '\n\n'.join([
     ])
 
 setup(name='plone.contentratings',
-      version="1.0.1-2",
+      version='1.1',
       description="Plone support for the contentratings package",
       long_description=description,
       classifiers=[
